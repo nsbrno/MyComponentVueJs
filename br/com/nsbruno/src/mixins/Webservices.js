6e4cb9df1e234404131397240f427cdd58ca21a4
Nogueira <ns.bruno@gmail.com>
2019-11-12 22:13:54 -0400

    Publish npm

--- br/com/nsbruno/src/mixins/Webservices.js
+++ br/com/nsbruno/src/mixins/Webservices.js
@@ -93,7 +93,7 @@ export default {
           axios
             .post(url, data, configuration)
             .then(function(response) {
-              //console.log('log webservice do then axios response: ' + JSON.stringify(response));
+              console.log('log webservice do then axios response: ' + JSON.stringify(response));
               // Checa o status do header do rest
               if (response.status === 200) {
                 resolve({
